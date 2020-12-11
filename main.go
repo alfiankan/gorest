3215ceaf100c96b7fd79d90fe69d7b18e3717d2a
Alfiankan <git@alfiankan.com>
2020-12-12 04:11:36 +0700

    xml encoding

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string
+	Name    string `json:"full_name" cml:"name"`
 	City    string
 	Zipcode string
 }
@@ -29,6 +30,13 @@ func getAllCustomer(w http.ResponseWriter, r *http.Request) {
 		{"Supardi", "Bandung", "577874"},
 	}
 	//tambah info di header kalau response berupa json app
-	w.Header().Add("Content-Type", "application/jsonß")
-	json.NewEncoder(w).Encode(customers)
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+
+	}
+
 }
