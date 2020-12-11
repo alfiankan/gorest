ee550b7a9023ccb0584e8343c44772a26db0b9a2
Alfiankan <git@alfiankan.com>
2020-12-12 03:35:48 +0700

    json encode

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string
+	City    string
+	Zipcode string
+}
+
 func main() {
 	http.HandleFunc("/greet", greet)
-
+	http.HandleFunc("/customer", getAllCustomer)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
+}
+
+func getAllCustomer(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Alfiankan", "Solo", "577871"},
+		{"Supardi", "Bandung", "577874"},
+	}
+	json.NewEncoder(w).Encode(customers)
 }
